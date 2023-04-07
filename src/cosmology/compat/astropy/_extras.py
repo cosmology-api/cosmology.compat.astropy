b36e6429901f84e48372f7cb3753146034ca0d4f
nstarman <nathanielstarkman@gmail.com>
2023-04-07 12:59:38 -0400

    docstrings
    Signed-off-by: nstarman <nathanielstarkman@gmail.com>

--- src/cosmology/compat/astropy/_extras.py
+++ src/cosmology/compat/astropy/_extras.py
@@ -6,12 +6,12 @@ from typing import TYPE_CHECKING
 
 import astropy.units as u
 import numpy as np
-from astropy.units import Quantity
 
 __all__: list[str] = []
 
 if TYPE_CHECKING:
     from astropy.cosmology import FLRW
+    from astropy.units import Quantity
 
     from cosmology.compat.astropy._core import NDFloating
 
@@ -27,33 +27,64 @@ _RHO_UNITS = u.solMass / u.Mpc**3
 
 
 class CriticalDensity:
+    """The cosmology has methods for the critical density."""
+
     cosmo: FLRW
 
     @property
     def critical_density0(self) -> Quantity:
+        """Critical density at z = 0 in Msol Mpc-3."""
         return self.cosmo.critical_density0.to(_RHO_UNITS)
 
     def critical_density(self, z: Quantity | NDFloating | float, /) -> Quantity:
+        """Redshift-dependent critical density in Msol Mpc-3."""
         return self.cosmo.critical_density(z).to(_RHO_UNITS)
 
 
 class HubbleParameter:
+    r"""The cosmology has methods to retrieve the Hubble parameter :math:`H`."""
+
     cosmo: FLRW
 
     @property
     def H0(self) -> Quantity:
+        """Hubble parameter at redshift 0 in km s-1 Mpc-1."""
         return self.cosmo.H0.to(_H0_UNITS)
 
     @property
     def hubble_distance(self) -> Quantity:
+        """Hubble distance in Mpc."""
         return self.cosmo.hubble_distance.to(u.Mpc)
 
     @property
     def hubble_time(self) -> Quantity:
+        """Hubble time in Gyr."""
         return self.cosmo.hubble_time.to(u.Gyr)
 
     def H(self, z: Quantity | NDFloating | float, /) -> Quantity:
+        """Hubble parameter :math:`H(z)` in km s-1 Mpc-1.
+
+        Parameters
+        ----------
+        z : Array
+            The redshift(s) at which to evaluate the Hubble parameter.
+
+        Returns
+        -------
+        Array
+        """  # noqa: D402
         return self.cosmo.H(z).to(_H0_UNITS)
 
     def h_over_h0(self, z: Quantity | NDFloating | float, /) -> Quantity:
+        """Standardised Hubble function :math:`E(z) = H(z)/H_0`.
+
+        Parameters
+        ----------
+        z : Array
+            The redshift(s) at which to evaluate.
+
+        Returns
+        -------
+        Array
+        """
         return np.asarray(self.cosmo.efunc(z)) << u.one
