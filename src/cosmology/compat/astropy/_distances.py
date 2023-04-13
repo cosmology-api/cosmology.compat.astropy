80620f4a2bcf73a22a1981935497cce406ec972b
nstarman <nathanielstarkman@gmail.com>
2023-04-13 12:01:45 -0700

    rename Tcmb
    Signed-off-by: nstarman <nathanielstarkman@gmail.com>

--- src/cosmology/compat/astropy/_distances.py
+++ src/cosmology/compat/astropy/_distances.py
@@ -54,11 +54,11 @@ class HasDistanceMeasures:
         return np.asarray(self.cosmo.scale_factor(z)) << u.one
 
     @property
-    def Tcmb0(self) -> Quantity:
+    def T_cmb0(self) -> Quantity:
         """CMB temperature in K at z=0."""
         return self.cosmo.Tcmb0.to(u.K)
 
-    def Tcmb(self, z: InputT, /) -> Quantity:
+    def T_cmb(self, z: InputT, /) -> Quantity:
         """CMB temperature in K at redshift z.
 
         Parameters
