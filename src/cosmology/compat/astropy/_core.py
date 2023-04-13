365479f497ad883bc706a5c1835ab1ef2d26794d
nstarman <nathanielstarkman@gmail.com>
2023-04-13 12:01:35 -0700

    cosmo namespace as attr
    Signed-off-by: nstarman <nathanielstarkman@gmail.com>

--- src/cosmology/compat/astropy/_core.py
+++ src/cosmology/compat/astropy/_core.py
@@ -29,22 +29,9 @@ class CosmologyWrapper(CosmologyWrapperAPI[Quantity, InputT]):
 
     cosmo: astropy_cosmology.Cosmology
 
-    def __cosmology_namespace__(
-        self, /, *, api_version: str | None = None
-    ) -> CosmologyNamespace:
+    @property
+    def __cosmology_namespace__(self) -> CosmologyNamespace:
         """Returns an object that has all the cosmology API functions on it.
-
-        Parameters
-        ----------
-        api_version: Optional[str]
-            string representing the version of the cosmology API specification
-            to be returned, in ``'YYYY.MM'`` form, for example, ``'2020.10'``.
-            If ``None``, it return the namespace corresponding to latest version
-            of the cosmology API specification.  If the given version is invalid
-            or not implemented for the given module, an error is raised.
-            Default: ``None``.
-
-            .. note:: currently only `None` is supported.
 
         Returns
         -------
